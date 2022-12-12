8336f71cb1103a0b2de2ba515ae151cea36a1239
josh-hga <josh.grime@hgacreative.com>
2022-12-12 20:11:02 +0000

    Day 7

--- Day 7/index.js
+++ Day 7/index.js
@@ -9,10 +9,10 @@ function parseData(data) {
 
     data = data.split('\n');
 
-    ChallengePart1(data);
+    MapSystem(data);
 }
 
-function ChallengePart1(lines) {
+function MapSystem(lines) {
 
     var fileSys = {
         '/': {
@@ -21,27 +21,36 @@ function ChallengePart1(lines) {
         }
     }
 
-    var dir = '/';
+    var dir = ['/'];
 
     function processInputCmd(cmd, index) {
 
         var parsed = cmd.split(' ');
-    
         var command = parsed[1];
 
         if (command === 'cd') {
         
-            var inpt = parsed[2];
-            var currentDirectory = dir.split('/');
+            var inpt = parsed[2];         
 
             if (inpt === '..') {
-                currentDirectory = currentDirectory.slice(0, -1);
-                dir = currentDirectory.join('/');
+                dir.length = dir.length - 1;
+
+            }
+            else {
+                dir.push(inpt);
             }
 
         }
 
         else if (command === 'ls') {
+
+            let depth = dir.length;
+            let objRef = fileSys['/'];
+
+            for (let x=1; x<depth; x++) {
+                objRef = objRef.subdirs[dir[x]];
+
+            }
 
             for (let i=index+1; i<lines.length; i++) {
 
@@ -49,16 +58,16 @@ function ChallengePart1(lines) {
 
                 else if (lines[i].substring(0,3) === 'dir') {
 
-                    var dirname = lines[i].split(' ')[1];
+                    var nextDir = lines[i].split(' ')[1];
 
-                    fileSys[dir].subdirs[dirname] = {
+                    objRef.subdirs[nextDir] = {
                         files: [],
                         subdirs: {}
                     };
 
                 }
                 else {
-                    fileSys[dir].files.push(lines[i]);
+                    objRef.files.push(lines[i]);
                 }
 
             }
@@ -66,20 +75,77 @@ function ChallengePart1(lines) {
         }
     
     }
-    
+
     for (let i=1; i<lines.length; i++) {
         if (lines[i][0] === '$') processInputCmd(lines[i], i);
     }
-
-
-}
-
-
-
-function processOutput() {
+    ChallengePart1(fileSys);
 
 }
 
 
+
+var challenge1Vals = [];
+
+function getDirSizeRecursive(dirRef){
+
+    dirRef.fileSize = 0;
+
+    for (let file of dirRef.files) {
+        var fileSize = parseInt(file.split(' ')[0]);
+        dirRef.fileSize += fileSize;
+    }
+
+    for (let dir in dirRef.subdirs) {
+            var subdirottal = getDirSizeRecursive(dirRef.subdirs[dir]);
+            dirRef.fileSize += subdirottal;
+    }
+
+    if (dirRef.fileSize <= 100000) challenge1Vals.push(dirRef.fileSize);
+
+    return dirRef.fileSize;
+
+}
+
+function ChallengePart1(sys){
+
+    getDirSizeRecursive(sys['/']);
+    
+    var solution = 0;
+
+    challenge1Vals.forEach(x=> {solution += x});
+
+    console.log('Part 1: The solution is '+solution);
+
+    ChallengePart2(sys);
+}
+
+function ChallengePart2(sys){
+    var max_space = 70000000;
+    var req_space = 30000000;
+    var used_space = sys['/'].fileSize;
+    var space_needed = req_space - (max_space - used_space);
+
+    var currentBest = req_space;
+
+    var sizes = [];
+
+    function checkSize(dirRef){
+        for (let dir in dirRef.subdirs) {
+            var filesize = dirRef.subdirs[dir].fileSize;
+
+            sizes.push(filesize);
+            if (filesize > space_needed && filesize < currentBest) {
+                currentBest = filesize;
+                
+            }
+           checkSize(dirRef.subdirs[dir]);
+        }
+    }
+    checkSize(sys['/']);
+
+    console.log('Part 2: Solution is '+currentBest);
+
+}
 
 readFile();
