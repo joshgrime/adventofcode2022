79c02ed07861908e380a41944029a7fa458532ed
josh-hga <josh.grime@hgacreative.com>
2022-12-14 19:26:16 +0000

    Day 9

--- Day 9/index.js
+++ Day 9/index.js
@@ -1,22 +1,18 @@
 var fs = require('fs');
 
 function readFile() {
-    var data = fs.readFileSync(__dirname+'/data2.txt', 'utf8');
+    var data = fs.readFileSync(__dirname+'/data.txt', 'utf8');
     parseData(data);
 }
 
 function parseData(data) {
 
-    var inputs = data.split('\r\n')
+    var inputs = data.split('\n')
                 .map(input => {
                     let i = input.split(' ');
                     return {direction: i[0], amount: parseInt(i[1])}
                 });
 
-
-   //inputs.length = 10;
-
-    console.log(inputs);
     ChallengePart1(inputs);
 
 }
@@ -28,9 +24,9 @@ function ChallengePart1(inputs) {
 
     var solution = [];
 
+    solution.push(JSON.stringify(tail));
+
     for (let input of inputs) {
-        
-        console.log(input);
 
         for (let i=0; i<input.amount; i++) {
 
@@ -38,72 +34,109 @@ function ChallengePart1(inputs) {
             if (input.direction === 'D') head.y++;
             if (input.direction === 'L') head.x--;
             if (input.direction === 'R') head.x++;
-            console.log('i: '+i);
-            var newTail = calcTailFollow(input.direction, head, tail);
+            
+            var newTail = calcTailFollow(head, tail);
+
             tail.x = newTail.x;
             tail.y = newTail.y;
-            console.log('Moved tail to');
-            console.log(tail);
+
+            var tailStr = JSON.stringify(newTail);
+            if (solution.indexOf(tailStr) === -1) solution.push(tailStr);
+
         }
 
-        
-        console.log('New head is: ');
-        console.log(head);
-        console.log('New tail is: ');
-        console.log(tail);
-        console.log('*****');
-
-        var tailStr = newTail.toString();
-
-        if (solution.indexOf(tailStr === -1)) solution.push(tailStr);
-        
     }
 
-    console.log('There are '+solution.length+' unique tail locations.');
+    console.log('Part 1: There are '+solution.length+' unique tail locations.');
+    ChallengePart2(inputs);
 
 }
 
-function calcTailFollow(direction, head, tail) {
+function ChallengePart2(inputs) {
 
-    console.log('Working with head:');
-    console.log(head);
+    var rope = [];
 
-    var negative = (direction === 'U' || direction === 'L') ? true : false;
+    for (let i=0; i<10; i++) {
+        rope.push({x: 50, y: 50});
+    }
+
+    var solution = [];
+
+    solution.push(JSON.stringify(rope[0]));
+
+    for (let input of inputs) {
+
+        for (let i=0; i<input.amount; i++) {
+
+            let head = rope[0];
+
+            if (input.direction === 'U') head.y--;
+            if (input.direction === 'D') head.y++;
+            if (input.direction === 'L') head.x--;
+            if (input.direction === 'R') head.x++;
+
+            for (let j=1; j<rope.length; j++) {
+
+                let knot = rope[j];
+                var newKnot = calcTailFollow(rope[j-1], knot);
+
+                rope[j].x = newKnot.x;
+                rope[j].y = newKnot.y;
+
+                if (j === rope.length-1) {
+                    var tailStr = JSON.stringify(newKnot);
+                    if (solution.indexOf(tailStr) === -1) solution.push(tailStr);
+                }
+
+            }
+
+        }
+
+    }
+    console.log('Part 2: There are '+solution.length+' unique tail locations');
+}
+
+function calcTailFollow(head, tail) {
+
+    if (tail.y === head.y && tail.x === head.x) return tail;
 
     var xdiff = head.x - tail.x;
     var ydiff = head.y - tail.y;
 
-    if (xdiff < 0) xdiff *= -1;
-    if (ydiff < 0) ydiff *= -1;
+    var pos_xdiff = xdiff < 0 ? xdiff * -1 : xdiff; 
+    var pos_ydiff = ydiff < 0 ? ydiff * -1 : ydiff;
 
-    console.log('xdiff: '+xdiff+'  |  ydiff: '+ydiff);
-    
-    if (xdiff > 1 || ydiff > 1) { // not touching, need to move
-        if (xdiff > 1 && ydiff <= 1) { // just move x
-        console.log('Need to move x')
-            if (negative) tail.x--;
-            else tail.x++;
-        } 
-        else if (ydiff > 1 && xdiff <= 1) {// just move y
-        console.log('Need to move y')
-            if (negative) tail.y--;
-            else tail.y++;
+    if (pos_xdiff <= 1 && pos_ydiff <= 1) return tail;
+
+    if ((pos_xdiff > 1 && pos_ydiff > 0) || (pos_xdiff > 0 && pos_ydiff > 1)) { //diagonal move
+
+        if (xdiff > 0 && ydiff > 0) { //down and right
+            tail.x++;
+            tail.y++;
         }
-        else { //move both
-        console.log('Need to move both')
-            if (negative) {
-                tail.x--;
-                tail.y--;
-            }
-            else {
-                tail.x++;
-                tail.y++;
-            }
+        if (xdiff > 0 && ydiff < 0) { //up and right
+            tail.x++;
+            tail.y--;
         }
+        if (xdiff < 0 && ydiff < 0) { //up and left
+            tail.x--;
+            tail.y--;
+        }
+        if (xdiff < 0 && ydiff > 0) { //down and left
+            tail.x--;
+            tail.y++;
+        }
+
     }
-    else {
-        console.log('Dont need to move');
+    else { //horizontal or vertical move
+
+        if (xdiff > 1) tail.x++;
+        if (xdiff < -1) tail.x--;
+        if (ydiff > 1) tail.y++;
+        if (ydiff < -1) tail.y--;
+        
     }
+
     return tail;
 
 }
